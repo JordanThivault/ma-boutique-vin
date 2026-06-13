fd10f884aec640a91719f6bd3f393467f280ad47
Jordan Thivault <jordan.thivault@gmail.com>
2026-06-13 06:40:07 +0200

    add analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Jost } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "sonner";
 
 import "./globals.css";
@@ -38,6 +39,7 @@ export default function RootLayout({
         {children}
 
         <Toaster position="top-right" richColors closeButton duration={3000} />
+        <Analytics />
       </body>
     </html>
   );
